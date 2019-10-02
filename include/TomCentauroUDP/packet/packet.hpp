bc2d66f8ce1fc1cec2dd666164bf1d769bbc1c4b
Luca Muratore <luca.muratore@iit.it>
2019-10-02 18:08:39 +0200

    working example in lo interface with UDP sender (through markers) and receiver

--- include/TomCentauroUDP/packet/packet.hpp
+++ include/TomCentauroUDP/packet/packet.hpp
@@ -1,167 +1,68 @@
 #ifndef PACKETS_H
 #define PACKETS_H
 
-
 namespace TomCentauroUDP {
     namespace packet {
-        struct master2slave {
-            float timer_master;
-            float timer_slave;
-            float run;
-            
-            // operation_mode: 0 cartesian, 1 joint space
-            int operation_mode;
-            
-            // cartesian space
-            double r_ref_position_x;
-            double r_ref_position_y;
-            double r_ref_position_z;
-            double r_ref_rotation[9]; // column major ordered
-            
-            // joint space
-            double r_ref_position_1;
-            double r_ref_position_2;
-            double r_ref_position_3;
-            double r_ref_position_4;
-            double r_ref_position_5;
-            double r_ref_position_6;
-
-            // right hand
-            float r_f0_ref_position;
-            float r_f1_ref_position;
-            float r_f2_ref_position;
-            float r_f3_ref_position;
-
-            
-        };
-
-        struct slave2master {
-            float timer_master;
-            float timer_slave;
-            float run;
-            
-            // operation_mode: 0 cartesian, 1 joint space
-            int current_operation_mode;
-            
-            // joint position and torque
-            double r_position_1;
-            double r_position_2;
-            double r_position_3;
-            double r_position_4;
-            double r_position_5;
-            double r_position_6;
-            double r_torque_1;
-            double r_torque_2;
-            double r_torque_3;
-            double r_torque_4;
-            double r_torque_5;
-            double r_torque_6;
-
-            // r is right
-            double r_position_x;
-            double r_position_y;
-            double r_position_z;
-            double r_rotation[9]; // column major ordered
-
-            // right hand: position, current, analaogs
-            float r_f0_position;
-            float r_f1_position;
-            float r_f2_position;
-            float r_f3_position;
-            
-            float r_f0_current;
-            float r_f1_current;
-            float r_f2_current;
-            float r_f3_current;
-
-            float r_f0_analogs_1;
-            float r_f0_analogs_2;
-            float r_f0_analogs_3;
-            
-            float r_f1_analogs_1;
-            float r_f1_analogs_2;
-            float r_f1_analogs_3;
-            
-            float r_f2_analogs_1;
-            float r_f2_analogs_2;
-            float r_f2_analogs_3;
-            
-            float r_f3_analogs_1;
-            float r_f3_analogs_2;
-            float r_f3_analogs_3;
-            
-            // right F/T
-            double force_x;
-            double force_y;
-            double force_z;
-            double torque_x;
-            double torque_y;
-            double torque_z;
-            
-        };
-
 
         ////////////////////////////////////////////////////////////////////////////////////////////
         /// Teleopman2ToM
         ////////////////////////////////////////////////////////////////////////////////////////////
 
-        struct Teleopman2ToM
+        struct __attribute__((__packed__))Teleopman2ToM
         {
         /// packet information
-            float timestamp_slave;   // time stamp of this packet
-            u_int packet_id;         // packet id
+            float timestamp_master;   // time stamp of this packet
+            float timestamp_slave;
+            int packet_id;           // packet id
 
-        /// control mode
-            int teleopman_mode; // the control mode of the teleop-man: 1 = cartesian impedance control, 2 = joint impedance control, 3. cartesian position/velocity control ....
+        /// actual control mode
+            int teleopman_mode; // operation_mode: 0 cartesian, 1 joint space
             int hand_mode;
 
         /// Cartesian Space EE Feedback
             double position_ee[3];   // current end-effector position of the Teleop-Man: frame{teleop_link5}
             double quaternion_ee[4]; // current end-effector orientation (expressed by quaternion) of the Teleop-Man: frame{teleop_link5}
-            double cmd_position_ee[3]; // last commanded position for the IK controller (input to IK solver)
-            double cmd_quaternion_ee[4]; // last commanded orientation for the IK controller (input to IK solver)
+            double ref_position_ee[3]; // last commanded position for the IK controller (input to IK solver)
+            double ref_quaternion_ee[4]; // last commanded orientation for the IK controller (input to IK solver)
 
         /// Joint Space Feedback
             double q[5];             // joint position of the Teleop-Man
             double torque[5];        // joint torque of the Teleop-Man
 
         /// F/T wrist sensor information
-            double force[3];         // the force (Cartesian) sensed by the F/T sensor installed on the wrist
-            double torque[3];        // the torque (Cartesian) sensed by the F/T sensor installed on the wrist
+            double ft_force[3];         // the force (Cartesian) sensed by the F/T sensor installed on the wrist
+            double ft_torque[3];        // the torque (Cartesian) sensed by the F/T sensor installed on the wrist
 
         /// Hand information
-            double torque_fingers[4]; // the actuator torque for each finger. (Do we have 4 fingers?)
+            double i_fingers[4]; // the actuator current (in Amper) for each finger.
             double q_fingers[4];      // the actuator position for each finger.
-            double position_tip_fingers1[3]; // the cartesian tip position of the first finger (Thumb)
-            double position_tip_fingers2[3]; // the cartesian tip position of the second finger (index)
-            double position_tip_fingers3[3]; // the cartesian tip position of the third finger (middle)
-            double position_tip_fingers4[3]; // the cartesian tip position of the fourth finger (ring?)
 
         /// Pressure hand sensor information
-            double pressure_fingers[11];    // the pressure sensor data installed on each phalanx
-        };
+            double pressure_fingers[12];    // the pressure sensor data installed on each phalanx
+        }; 
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
         /// ToM2Teleopman
         ////////////////////////////////////////////////////////////////////////////////////////////
 
-        struct ToM2Teleopman
+        struct __attribute__((__packed__))ToM2Teleopman
         {
         /// packet information
-            float timestamp_slave;   // time stamp of this packet
-            u_int packet_id;         // packet id
+            float timestamp_master;   // time stamp of this packet
+            float timestamp_slave; 
+            int packet_id;            // packet id
 
         /// Cartesian Space EE Feedback
-            double position_ee[3];   // end-effector position of the Teleop-Man: frame{teleop_link5}
-            double quaternion_ee[4]; // end-effector orientation of the Teleop-Man. expressed in quaternion
+            double ref_position_ee[3];   // end-effector position of the Teleop-Man: frame{teleop_link5}
+            double ref_quaternion_ee[4]; // end-effector orientation of the Teleop-Man. expressed in quaternion
 
         /// Joint Space Feedback
-            double q[5];             // joint position of the Teleop-Man
-            double torque[5];        // joint torque of the Teleop-Man
+            double ref_q[5];             // joint position of the Teleop-Man
+            double ref_torque[5];        // joint torque of the Teleop-Man
 
         /// Hand information
-            double closure_cmd_fingers[4];  // how do we control the fingers?
+            double ref_closure_fingers[4];  // how do we control the fingers? NOTE Percentage
 
         };
     }
---
+++ src/udp_receiver.cpp
@@ -0,0 +1,173 @@
+/*
+    Simple udp slave
+*/
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include <unistd.h>
+#include <sched.h>
+
+#include <TomCentauroUDP/packet/packet.hpp>
+
+#include <XBotInterface/Utils.h>
+#include <XBotCore-interfaces/XDomainCommunication.h>
+
+#define ADDRESS_OTHER "127.0.0.1"
+#define ADDRESS_ME "127.0.0.1"
+
+#define BUFLEN_MASTER_2_SLAVE sizeof(TomCentauroUDP::packet::ToM2Teleopman)
+#define BUFLEN_SLAVE_2_MASTER sizeof(TomCentauroUDP::packet::Teleopman2ToM)
+
+#define PORT_ME 5000   //The port on which to listen for incoming data
+#define PORT_OTHER 5001   //The port on which to listen for incoming data
+
+char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen)
+{
+    switch(sa->sa_family) {
+        case AF_INET:
+            inet_ntop(AF_INET, &(((struct sockaddr_in *)sa)->sin_addr),
+                    s, maxlen);
+            break;
+
+        case AF_INET6:
+            inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)sa)->sin6_addr),
+                    s, maxlen);
+            break;
+
+        default:
+            strncpy(s, "Unknown AF", maxlen);
+            return NULL;
+    }
+
+    return s;
+}
+
+int main ( void ) {
+
+    // UDP related stuffs
+    struct sockaddr_in si_me, si_other, si_recv;
+    int s_recv, s_send, i , recv_len;
+    uint slen = sizeof ( si_other );
+
+    // select config
+    fd_set cset;
+    struct timeval tval;
+    tval.tv_sec = 0;
+    tval.tv_usec = 2000; //2ms
+    //keep listening for data
+
+    // master to slave packet
+    struct TomCentauroUDP::packet::ToM2Teleopman *pkt_master_to_slave =  (TomCentauroUDP::packet::ToM2Teleopman *) malloc ( BUFLEN_MASTER_2_SLAVE );
+    // slave to master packet
+    struct TomCentauroUDP::packet::Teleopman2ToM *pkt_slave_to_master =  (TomCentauroUDP::packet::Teleopman2ToM *) malloc ( BUFLEN_SLAVE_2_MASTER );
+
+    // exoskeleton pipe
+    XBot::PublisherNRT<TomCentauroUDP::packet::ToM2Teleopman> exoskeleton_pub("exoskeleton_pipe");
+    // robot pipe
+    XBot::SubscriberNRT<TomCentauroUDP::packet::Teleopman2ToM> robot_sub("robot_pipe");
+
+    //create a UDP socket
+    if ( ( s_recv=socket ( AF_INET, SOCK_DGRAM, IPPROTO_UDP ) ) == -1 ) {
+        XBot::Logger::error ( "socket\n" );
+        exit(1);
+    }
+    
+//     TBD check if needed
+     struct timeval read_timeout;
+     read_timeout.tv_sec = 0;
+     read_timeout.tv_usec = 1000;
+//     setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
+
+    //create send UDP socket
+    if ( ( s_send=socket ( AF_INET, SOCK_DGRAM, IPPROTO_UDP ) ) == -1 ) {
+        XBot::Logger::error ( "socket\n" );
+        exit(1);
+    }
+
+    // zero out the structure
+    memset ( ( char * ) &si_me, 0, sizeof ( si_me ) );
+
+    // initialize address to bind
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons ( PORT_ME );
+    si_me.sin_addr.s_addr = inet_addr ( ADDRESS_ME );
+
+    //bind socket to port
+    if ( bind ( s_recv , ( struct sockaddr* ) &si_me, sizeof ( si_me ) ) == -1 ) {
+        XBot::Logger::error ( "bind\n" );
+    }
+
+    // initialize master address
+    memset ( ( char * ) &si_other, 0, sizeof ( si_other ) );
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons ( PORT_OTHER );
+    if ( inet_aton ( ADDRESS_OTHER , &si_other.sin_addr ) == 0 ) {
+        XBot::Logger::error ( "inet_aton() failed\n" );
+        exit (1);
+    }
+    
+    char t[1000];
+    std::cout << "OTHER: " << get_ip_str(( struct sockaddr * ) &si_other, t, 1000) << ":" << ntohs( si_other.sin_port) << std::endl;
+    std::cout << "ME: " << get_ip_str(( struct sockaddr * ) &si_me, t, 1000) << ":" << ntohs( si_me.sin_port) << std::endl;
+
+
+    int count = 0;
+    int retry = 0;
+    
+    // clear the data struct
+    memset ( pkt_master_to_slave, 0, sizeof ( *pkt_master_to_slave ) );
+    memset ( pkt_slave_to_master, 0, sizeof ( *pkt_slave_to_master ) );
+    
+    //keep listening for data
+    while ( 1 ) {
+        
+        // read from robot pipe
+        int bytes = robot_sub.read(*pkt_slave_to_master);
+        
+        if(bytes == 0) {
+            usleep(100000);
+            continue;
+        }
+        
+        std::cout << pkt_slave_to_master << std::endl;
+        
+        //send the message
+        if ( sendto ( s_send, pkt_slave_to_master, BUFLEN_SLAVE_2_MASTER , 0 , ( struct sockaddr * ) &si_other, slen ) == -1 ) {
+            perror("sendto()");
+            exit(1);
+        }
+
+        FD_ZERO ( &cset );
+        FD_SET ( s_recv, &cset );
+        int n = select ( FD_SETSIZE, &cset, NULL, NULL, &tval );
+        if ( n == -1 ) {
+            XBot::Logger::error ( "select() failed\n" );
+        }
+        if ( FD_ISSET ( s_recv, &cset ) ) {
+            //try to receive some data, this is a blocking call
+            if ( ( recv_len = recvfrom ( s_recv, pkt_master_to_slave, BUFLEN_MASTER_2_SLAVE, 0, ( struct sockaddr * ) &si_recv, &slen ) ) == -1 ) {
+                XBot::Logger::error ( "recvfrom()\n" );
+            }
+            else {
+                
+                // write on exoskeleton_pipe
+                exoskeleton_pub.write(*pkt_master_to_slave);
+            }
+        }
+        else {
+            //TBD?
+        }
+
+
+
+        // TBD check!
+        usleep(1000); // 1 ms
+        count++;
+    }
+
+    close ( s_recv );
+    close ( s_send );
+    return 0;
+}
