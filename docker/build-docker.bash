0f7ece5f845776a3f2f4d26e04c3a365e9c10100
Siddharth Deore <siddharth.deore@iit.it>
2022-10-28 17:45:44 +0200

    updated docker image tag.

--- docker/build-docker.bash
+++ docker/build-docker.bash
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
-docker build --pull --tag tom_centauro:latest . -f $DIR/Dockerfile
+docker build --pull --tag arturolaurenzi/tom_centauro:latest . -f $DIR/Dockerfile
