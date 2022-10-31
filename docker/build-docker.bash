b6d1a3d70f881316386cf73854dcbf10fba9f695
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-10-31 14:43:32 +0100

    docker

--- docker/build-docker.bash
+++ docker/build-docker.bash
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
-docker build --pull --tag arturolaurenzi/tom_centauro:latest . -f $DIR/Dockerfile
+docker build --progress=plain --pull --tag arturolaurenzi/tom_centauro:latest . -f $DIR/Dockerfile
