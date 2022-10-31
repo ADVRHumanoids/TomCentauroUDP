645aa86ff29537396867ad0b46775082fdc14125
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-10-31 13:22:37 +0100

    pkt: velocity control flag, renamed fields for clarity

--- include/tom_centauro_udp/packet/packet.hpp
+++ include/tom_centauro_udp/packet/packet.hpp
@@ -11,28 +11,32 @@ namespace tom_centauro_udp { namespace packet {
  */
 struct __attribute__((packed)) master2slave
 {
-    float timer_master;
-    float timer_slave;
-    bool run;  // should slave execute this command?
+    float timer_master = 0;
+    float timer_slave = 0;
+    bool run = false;  // should slave execute this command?
 
     // ee URDF name
     char ee_id[16];  // up to 15 chars + null-termination
 
+    // if true, velocity commands are sent, and position
+    // is disregarded (useful for moving the base)
+    bool velocity_ctrl = false;
+
     // ee pose command
-    float position_x;
-    float position_y;
-    float position_z;
-    float rotation[4];      // quaternion (w, xi, yj, zk)
+    float position_x = 0;
+    float position_y = 0;
+    float position_z = 0;
+    float rotation[4];  // quaternion (w, xi, yj, zk)
 
     // hand command (desired pos, desired force?)
-    float gripper_pos;
-    float gripper_force;
+    float gripper_pos = 0;
+    float gripper_force = 0;
     
-    // jostick commands
-    bool jst_button[3];
-    float jst_axes[2];      // raw joystick x,y data [-1 to 1]
-    float aux_heading;      // posiible centauro heading command (rotatate about z axis)
-    float aux_pos[2];       // posible centtaoro velocity/direction command (x,y)
+    // velocity commands
+    bool jst_button[3];  // used for what ?
+    float vel_xy[2];  // raw joystick x,y data [-1 to 1]
+    float vel_yaw;  // possible centauro heading command (rotatate about z axis)
+    float aux_pos[2];  // possible centauro velocity/direction command (x,y)
 
     // simple check code
     uint32_t magic_code = expected_magic_code;
